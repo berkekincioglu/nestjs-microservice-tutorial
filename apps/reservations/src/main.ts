5ddbccff10677b29fed8c8d1b5464d7d5a687cf3
Berk Ekincioglu <berkekincioglu@MacBook-Pro-2.local>
2023-10-30 15:58:20 +0300

    #2

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -1,13 +1,12 @@
 import { NestFactory } from '@nestjs/core';
-
 import { ReservationsModule } from './reservations.module';
+import { ValidationPipe } from '@nestjs/common';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
-  //   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  //   app.useLogger(app.get(Logger));
-  //   app.use(cookieParser());
-  //   const configService = app.get(ConfigService);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useLogger(app.get(Logger));
   await app.listen(3000);
 }
 bootstrap();
